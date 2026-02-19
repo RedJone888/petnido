f835f981c73bf84fc9af81cd829722a242b11ae0
RedJone888 <redjoan.guo@gmail.com>
2026-02-19 18:00:22 +0900

    fix nextauth build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
 //NextAuth server路由
 import { handlers } from "@/server/auth/auth";
 export const { GET, POST } = handlers;
